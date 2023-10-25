5b52d3f42edf601e82c46f5439e835bf271abf7d
sriram9573 <94070892+sriram9573@users.noreply.github.com>
2023-10-24 17:24:46 -0700

    Update EffortLoggerConsoleController.java

--- EffortLoggerConsoleController.java
+++ EffortLoggerConsoleController.java
@@ -1,16 +1,27 @@
 package application;
 
+
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 
+
 public class EffortLoggerConsoleController {
+	
+	@FXML
+	private Rectangle ClockLabelRectangle;//sriram 
+	@FXML
+	private Label ClockLabel;//sriram
 	
 	private Stage stage;
 	private Scene scene;
@@ -43,5 +54,11 @@ public class EffortLoggerConsoleController {
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
+	}
+	@FXML
+	void start(ActionEvent event) throws IOException{//written by sriram
+		ClockLabelRectangle.setFill(Color.GREEN);//changes the clock is stopped label to green 
+		ClockLabel.setText("Clock is Running");//and the text is change to the clock is running  
+												//when the start the activity button is pressed.
 	}
 }
