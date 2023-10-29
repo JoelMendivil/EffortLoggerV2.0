7a00a339360ee761236828a3d4a95f5076033a91
KMaullboy <112103220+KMaullboy@users.noreply.github.com>
2023-10-28 18:51:08 -0700

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,8 @@
+module EffortLoggerV2 {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires junit;
+	requires org.junit.jupiter.api;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
