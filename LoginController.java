f2f2d69add2382e20e46130f92d3d0c550da9d75
sriram9573 <94070892+sriram9573@users.noreply.github.com>
2023-10-29 18:04:37 -0700

    Update LoginController.java

--- LoginController.java
+++ LoginController.java
@@ -31,7 +31,32 @@ public class LoginController {
     public void loginButtonAction(ActionEvent event) throws IOException {
         String username = usernameField.getText();
         String password = passwordField.getText();
-
+        if (username.isEmpty()&&password.isEmpty()) {
+        	messageLabel.setLayoutX(235);
+        	messageLabel.setLayoutY(150);
+        	messageLabel.setText("Both fields are empty");
+        	usernameField.setStyle("-fx-border-color: red; -fx-border-width: 2px;"); // Set the background color (optional)
+        	passwordField.setStyle("-fx-border-color: red; -fx-border-width: 2px;");
+        }
+        else if(username.isEmpty())
+        {
+        	messageLabel.setLayoutX(235);
+        	messageLabel.setLayoutY(150);
+        	messageLabel.setText("username field is empty");
+        	usernameField.setStyle("-fx-border-color: red; -fx-border-width: 2px;");
+        	passwordField.setStyle("-fx-border-color: white; -fx-border-width: 0px;");
+        }      
+        else if(password.isEmpty()) {
+        	messageLabel.setLayoutX(235);
+        	messageLabel.setLayoutY(150);
+        	messageLabel.setText("password field is empty");
+        	usernameField.setStyle("-fx-border-color: white; -fx-border-width: 0px;");
+        	passwordField.setStyle("-fx-border-color: red; -fx-border-width: 2px;");
+        }
+        else {
+        	usernameField.setStyle("-fx-border-color: white; -fx-border-width: 0px;"); 
+        	passwordField.setStyle("-fx-border-color: white; -fx-border-width: 0px;");
+        if(username.length()<17&&password.length()<10) {
         if (isValidCredentials(username, password)) {
         	messageLabel.setLayoutX(245);
         	messageLabel.setLayoutY(150);
@@ -47,13 +72,44 @@ public class LoginController {
         	messageLabel.setText("Invalid Credentials");
             usernameField.clear();
             passwordField.clear();
-        }
+        }}else {
+        	usernameField.clear();
+            passwordField.clear();
+        	messageLabel.setLayoutX(40);
+        	messageLabel.setLayoutY(150);
+        	messageLabel.setText("Please make sure your username and password contains less than 16 and 10 characters respectively");
+        } }
+        	
     }
 
     public void registerButtonAction() {
         String username = usernameField.getText();
         String password = passwordField.getText();
-
+        if (username.isEmpty()&&password.isEmpty()) {
+        	messageLabel.setLayoutX(235);
+        	messageLabel.setLayoutY(150);
+        	messageLabel.setText("Both fields are empty");
+        	usernameField.setStyle("-fx-border-color: red; -fx-border-width: 2px;"); // Set the background color (optional)
+        	passwordField.setStyle("-fx-border-color: red; -fx-border-width: 2px;");
+        }
+        else if(username.isEmpty())
+        {
+        	messageLabel.setLayoutX(235);
+        	messageLabel.setLayoutY(150);
+        	messageLabel.setText("username field is empty");
+        	usernameField.setStyle("-fx-border-color: red; -fx-border-width: 2px;");
+        	passwordField.setStyle("-fx-border-color: white; -fx-border-width: 0px;");
+        }      
+        else if(password.isEmpty()) {
+        	messageLabel.setLayoutX(235);
+        	messageLabel.setLayoutY(150);
+        	messageLabel.setText("password field is empty");
+        	usernameField.setStyle("-fx-border-color: white; -fx-border-width: 0px;");
+        	passwordField.setStyle("-fx-border-color: red; -fx-border-width: 2px;");
+        }else {
+        	usernameField.setStyle("-fx-border-color: white; -fx-border-width: 0px;");
+        	passwordField.setStyle("-fx-border-color: white; -fx-border-width: 0px;");
+        if(username.length()<17 || password.length()<10) {
         if (!username.isEmpty() && !password.isEmpty()) {
             try (PrintWriter writer = new PrintWriter(new FileWriter("credentials.txt", true))) {
                 writer.println(username + ":" + password);
@@ -71,7 +127,11 @@ public class LoginController {
         	messageLabel.setLayoutX(200);
         	messageLabel.setLayoutY(150);
         	messageLabel.setText("Please enter a username and password");
-        }
+        }}else {
+        	messageLabel.setLayoutX(40);
+        	messageLabel.setLayoutY(150);
+        	messageLabel.setText("Please make sure your username and password contains less than 16 and 10 characters respectively");
+        } }
     }
 
     private boolean isValidCredentials(String username, String password) {
