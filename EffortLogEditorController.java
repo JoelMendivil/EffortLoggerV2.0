06a725b400b65678ab92dd25230d6400d1ccb648
sriram9573 <94070892+sriram9573@users.noreply.github.com>
2023-11-11 16:54:28 -0700

    Update EffortLogEditorController.java

--- EffortLogEditorController.java
+++ EffortLogEditorController.java
@@ -1,12 +1,15 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 
@@ -15,6 +18,8 @@ public class EffortLogEditorController {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
+	@FXML
+    private ChoiceBox<String> choice_editor;
 	
 	public void switchToEffortLoggerConsole(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("EffortLoggerConsole.fxml"));
@@ -23,5 +28,17 @@ public class EffortLogEditorController {
 		stage.setScene(scene);
 		stage.show();
 	}
+	public void loadChoices() {
+		choice_editor.getItems().clear();
+        try (BufferedReader br = new BufferedReader(new FileReader("out.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+            	choice_editor.getItems().add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
 
 }
