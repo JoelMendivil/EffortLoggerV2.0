ab5a07071e9d6522648c29f305def93b45f69537
sriram9573 <94070892+sriram9573@users.noreply.github.com>
2023-11-12 15:06:13 -0700

    Update EffortLogEditorController.java

--- EffortLogEditorController.java
+++ EffortLogEditorController.java
@@ -21,6 +21,9 @@ public class EffortLogEditorController {
 	@FXML
     private ChoiceBox<String> choice_editor;
 	
+	@FXML
+    private ChoiceBox<String> delete_log;
+	
 	public void switchToEffortLoggerConsole(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("EffortLoggerConsole.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -28,17 +31,46 @@ public class EffortLogEditorController {
 		stage.setScene(scene);
 		stage.show();
 	}
+
 	public void loadChoices() {
+	
 		choice_editor.getItems().clear();
+		//delete_log.getItems().clear();
         try (BufferedReader br = new BufferedReader(new FileReader("out.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
             	choice_editor.getItems().add(line);
+            	//delete_log.getItems().add(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 	
-
+	public void loadChoices2() {
+		
+		delete_log.getItems().clear();
+		//delete_log.getItems().clear();
+        try (BufferedReader br = new BufferedReader(new FileReader("out.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+            	delete_log.getItems().add(line);
+            	//delete_log.getItems().add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	@FXML
+    private void removeSelectedItem(ActionEvent event) {
+        String selectedItem = delete_log.getValue();
+       
+        if (selectedItem != null) {
+            choice_editor.getItems().remove(selectedItem);
+        	delete_log.getItems().remove(selectedItem);
+        	delete_log.setStyle(" ");
+        	choice_editor.setStyle(" ");
+        }
+        
+    }
 }
