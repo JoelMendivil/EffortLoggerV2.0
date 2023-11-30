2306a1b609c5f73c4c0c9f04a4a15314e1a1e877
asap-kc <148286409+asap-kc@users.noreply.github.com>
2023-11-29 21:50:27 -0700

    Add files via upload
    updated commit with sriram and kevin's code working together for effortlogEditor

--- EffortLogEditorController.java
+++ EffortLogEditorController.java
@@ -1,8 +1,15 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -14,7 +21,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 
@@ -28,6 +38,41 @@ public class EffortLogEditorController {
 	
 	@FXML
     private ChoiceBox<String> delete_log;
+	
+	@FXML
+	private ChoiceBox<String> project ;
+
+	@FXML
+	private ChoiceBox<String> lifecycle;
+	@FXML
+	private ChoiceBox<String> effortcategory;
+	@FXML
+	private ChoiceBox<String> deliverable;
+	
+	@FXML 
+	private Label finished;
+	
+	@FXML
+	private Button delete;
+	
+	@FXML
+	private TextField date;
+	
+	@FXML
+	private TextField startTime;
+	
+	@FXML
+	private TextField stopTime;
+	
+	@FXML
+	private String newEntry;
+	
+	@FXML
+	private LocalTime stopTime2;
+	
+	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+	
+	public String midNight = "00:00:00";
 	
 	public void switchToEffortLoggerConsole(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("EffortLoggerConsole.fxml"));
@@ -53,36 +98,115 @@ public class EffortLogEditorController {
     }
 	
 	public void loadChoices2() {
-		
 		delete_log.getItems().clear();
 		//delete_log.getItems().clear();
-        try (BufferedReader br = new BufferedReader(new FileReader("out.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("projectFiles.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
             	delete_log.getItems().add(line);
-            	//delete_log.getItems().add(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+	
+	
 	@FXML
-    private void removeSelectedItem(ActionEvent event) {
-       
+	 private void removeSelectedItem(ActionEvent event) {
 		 String selectedItem = delete_log.getSelectionModel().getSelectedItem();
 		    if (selectedItem != null) {
 		        // Read the current choices from the file, remove the selected item, and write back
 		        updateFileContents("out.txt", selectedItem);
 		    }
 		    String selectedItem1 = delete_log.getValue();
-       
+
         if (selectedItem1 != null) {
             choice_editor.getItems().remove(selectedItem1);
         	delete_log.getItems().remove(selectedItem1);
         	delete_log.setStyle(" ");
         	choice_editor.setStyle(" ");
         }
+
     }
+	
+	
+	@FXML
+	public void updateTime(ActionEvent event)throws IOException {
+		String del = choice_editor.getValue().toString();
+		//Kevin
+				String selected = LocalDate.now().toString() +" (" + (startTime.getText().toString()) + "-" + (stopTime.getText().toString())+")"+ "null" +";"+ "null" +";"+ "null";
+		        try (PrintWriter out = new PrintWriter(new FileWriter("out.txt", true))) {
+		            out.println(selected);
+			}
+		    finished.setText("These attributes have been saved.");
+		    removePrev("out.txt", del);
+		    System.out.println(del);
+	}
+	
+	@FXML
+	public void splitLog(ActionEvent event)throws IOException {
+		String del = choice_editor.getValue().toString();
+		String ss = date.getText().toString();
+		stopTime2 = LocalTime.now();
+		//Kevin
+		String selected =  ss +" (" + (startTime.getText().toString()) + "-" + (midNight)+")"+ "null" +";"+ "null" +";"+ "null";
+	    try (PrintWriter out = new PrintWriter(new FileWriter("out.txt", true))) {
+        	out.println(selected);
+	   }
+	    
+	    String selected_ =  ss +" (" + (midNight) + "-" + stopTime2.format(formatter)+")"+ "null" +";"+ "null" +";"+ "null";
+	    try (PrintWriter out = new PrintWriter(new FileWriter("out.txt", true))) {
+        	out.println(selected_);
+	   }
+	    finished.setText("These. attributes have been saved.");
+	    removePrev("out.txt", del);
+	    System.out.println(del);
+	}
+	
+	
+	
+	//written by kevin cabrera to update time stamps on entries and help split logs
+	public void removePrev(String fileName, String delV ){
+		String tempFile = "temp.txt";
+		File oldFile = new File(fileName);
+		File newFile = new File(tempFile);
+		
+		String currentLine;
+		try {
+			FileWriter fw = new FileWriter(tempFile, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			PrintWriter pw = new PrintWriter(bw);
+			
+			FileReader fr = new FileReader(fileName);
+			BufferedReader br = new BufferedReader(fr);
+			
+			while((currentLine = br.readLine()) != null) {
+				if(!((currentLine.toString()).equals(delV))) {
+					pw.println(currentLine);
+					// searches for the desired entry to delete
+					// writes everything but that line
+				}
+			}
+			
+			pw.flush();
+			pw.close();
+			fr.close();
+			br.close();
+			bw.close();
+			fw.close();
+			
+			oldFile.delete();
+			File newOut = new File("out.txt");
+			newFile.renameTo(newOut);
+			//rename temp to out
+		}
+		catch(Exception e) {
+			System.out.println(e);
+			//exception handle
+		}
+	}
+	
+	
 	private void updateFileContents(String fileName, String itemToRemove) {
 	    try {
 	        List<String> lines = new ArrayList<>(Files.readAllLines(Paths.get(fileName)));
@@ -92,4 +216,5 @@ public class EffortLogEditorController {
 	        e.printStackTrace();
 	    }
 	}
+	
 }
--- module-info.java
+++ module-info.java
@@ -1,8 +1,8 @@
-module EffortLoggerV2 {
+module effort_Logger_V2 {
 	requires javafx.controls;
 	requires javafx.fxml;
-	requires junit;
-	requires org.junit.jupiter.api;
+	//requires junit;
+	//requires org.junit.jupiter.api;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
